f297bc62e0917cf5dbdf6d014aa53963e49fe310
Naruse Motoki <motoki@naru.se>
2014-09-20 22:34:56 +0900

    Add License test to head of el file

--- preview-lightweight-markup-language.el
+++ preview-lightweight-markup-language.el
@@ -1,3 +1,37 @@
+;;; preview-lightweight-markup-language.el --- Lightweight Markup Languages easy to preview.
+
+;; Filename: preview-lightweight-markup-language.el
+;; Description: Multiple scratches manager
+;; Author: Motoki Naruse <motoki@naru.se>
+;; Maintainer: Motoki Naruse <motoki@naru.se>
+;; Copyright (C) 2014, Motoki Naruse, all rights reserved.
+;; Created: 2014-09-17
+;; Version: 0.1
+;; Last-Updated: 2014-09-20
+;;           By: Motoki Naruse
+;; URL: https://github.com/narusemotoki/preview-lightweight-markup-language
+;; Keywords:
+;; Compatibility: GNU Emacs 24
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
 (setq plml-directory (file-name-directory (or load-file-name (buffer-file-name))))
 (setq plml-output-html-path "/tmp/plml.html")
 (setq plml-output-html-file-path (concat "file://" plml-output-html-path))
